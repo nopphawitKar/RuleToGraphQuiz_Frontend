12ec750088f3655e76f835e653e429bc5c05e605
DESKTOP-1BAA9UP\katen <nopphawit@mbc.co.th>
2019-04-07 03:18:20 +0700

    dont use partition_layout any more! just create our own style table tool

--- src/d3_util/Util_table_tool.js
+++ src/d3_util/Util_table_tool.js
@@ -1,6 +1,10 @@
 import * as d3 from "d3";
+import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
 
+const BOX_WIDTH = 100;
+const BOX_HIGHT = 100;
+const ORIGINAL_DEPTH = 0;
 // var graphData = [100,2,3];
 // var selectData = ['softdrink', 'cola', 'paper']
 
@@ -9,20 +13,23 @@ export function create(treeData, selector, updater) {
 			width = 960 - margin.left - margin.right,
 			height = 500 - margin.top - margin.bottom;
 
-	var partitionLayout = d3.partition()
-	  .size([400, 200]);
 	var treeDataCopy = Object.assign({}, treeData);
 	var rootNode = d3.hierarchy(treeDataCopy)
 
 	rootNode.sum(function(d) {
 	  // return d.value;
-		return 100;
+		var fakeValue = 1;
+		return fakeValue;
 	});
 
+	function initiate(depth) {
+		var margin = {top: 20, right: 90, bottom: 30, left: 90},
+				width = 960 - margin.left - margin.right,
+				height = 500 - margin.top - margin.bottom;
 
-	partitionLayout(rootNode);
+		var	x = scaleLinear().range([0, width]),
+	      y = scaleLinear().range([0, height]);
 
-	function reset(depth){
 		d3.select(selector).append('svg')
 			.attr("width", width + margin.right + margin.left)
 			.attr("height", height + margin.top + margin.bottom)
@@ -32,10 +39,8 @@ export function create(treeData, selector, updater) {
 		  .enter()
 		  .append('rect')
 			.attr('class', 'tabletool_top')
-		  .attr('x', function(d) { return d.x0; })
-		  .attr('y', function(d) { return d.y0; })
-		  .attr('width', function(d) { return 80; })
-		  .attr('height', function(d) { return d.y1 - d.y0; })
+		  .attr('width', function(d) { /*return x(d.x1 - d.x0); }*/return BOX_WIDTH})
+		  .attr('height', function(d) { /*return y(d.y1 - d.y0);*/return BOX_HIGHT })
 			.attr("id", function(d,i){ return "tabletool" + i})
 			.style("display", function(d) {
 		    if (d.depth > depth) {
@@ -43,54 +48,70 @@ export function create(treeData, selector, updater) {
 		    }
 		  })
 	}
-	reset(0);
 
+	function resetCoordinate(depth) {
+		d3.selectAll('.tabletool_top')
+		.attr('x', (node) => {
+			if (node.depth > depth) {
+				return;
+			}
+			return node.x;
+		})
+		.attr('y', (node) => {
+			if (node.depth > depth) {
+				return;
+			}
+			return node.x;
+		})
+	}
+
+
+	function onTabletoolClick(){
 		d3.selectAll('.tabletool_top').on('click', function(d) {
-			//click on a child
-			// var clickedX = d3.select(this).attr('x');
-			// var clickedY = d3.select(this).attr('y');
-			// var siblink = d.parent.children;
-			//
-			// siblink.forEach((each) => {
-			// 	var eachX = each.x0;
-			// 	var eachY = each.y0;
-			// 	if(clickedX != eachX){
-			// 		console.log(each.x0)
-			// 	}
-			// })
-
 			//click on a parent
 			var parent = d;
 			d3.selectAll('.tabletool_top')
 			.style("display", (node) => {
-				if(node.depth == parent.depth && node.x0 != parent.x0){
+				if(node.depth == parent.depth && node.data.name != parent.data.name){//node.x0 != parent.x0){
 					return "none";
 				}
 			})
-			 .attr('x', (node) => {
-				if(node.depth == parent.depth && node.x0 == parent.x0){
- 					return 0;
- 				}else{
-					return node.x0;
-				}
-			 })
+
+			//show all children
 			var children = d.children || [];
+			var childCounter = 0;
 			children.forEach((child) => {
-				var childX = child.x0;
+				// var childX = child.x0;
+				var childName = child.data.name;
 				var childDepth = child.depth;
 				//show all the clicked node's children
-				d3.selectAll('.tabletool_top').style("display", (node) => {
-			    if (node.depth == childDepth && node.x0 == childX) {
-			      return "";//nodes whose depth is more than 1 make its vanish
+				d3.selectAll('.tabletool_top')
+				.style("display", (node) => {
+			    if (node.depth == childDepth && node.data.name == childName) {
+			      return "";
 			    }
 			  })
+				.attr('x', (node) => {
+					if (node.depth == childDepth && node.data.name == childName) {
+						node.x = childCounter * BOX_WIDTH;
+			      return childCounter * BOX_WIDTH;
+			    }
+					return node.x;
+				})
+				.attr('y', (node) => {
+					if (node.depth == childDepth && node.data.name == childName) {
+						node.y = childDepth * BOX_HIGHT;
+			      return childDepth * BOX_HIGHT;
+			    }
+					return node.y;
+				})
+				childCounter++;
 			})
-
-		  // console.log('x: '+d3.select(this).attr('x')+' y: '+d3.select(this).attr('y')); // Logs the id attribute.
-			// console.log(d3.select(this).attr('x'))
-			// console.log(d.parent)
 		});
+	}
 
+	initiate(ORIGINAL_DEPTH);
+	onTabletoolClick();
 
 
 	// var margin = {top: 20, right: 90, bottom: 30, left: 90},
